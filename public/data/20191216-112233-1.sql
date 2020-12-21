0de4bf89ac4df8914da4a08f4dcfd7ffa551ac20
“blue-bxy <b2694645542@163.com>
2020-12-21 12:22:37 +0800

    first commit

---
+++ public/data/20191216-112233-1.sql
@@ -0,0 +1,13 @@
+-- -----------------------------
+-- Think MySQL Data Transfer 
+-- 
+-- Host     : 127.0.0.1
+-- Port     : 3306
+-- Database : money
+-- 
+-- Part : #1
+-- Date : 2019-12-16 11:22:34
+-- -----------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;
+
